513c50b74796a8ca251065339af0e5ad4c69ef72
UltrafunkAmsterdam <leon@ultrafunk.nl>
2021-10-19 22:24:20 +0200

    1.0.6

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.install_scripts import install_scripts
 import pathlib
 import re
+import os
+
 
 here = pathlib.Path(__file__).parent.resolve()
 content = (here / 'README.md').read_text(encoding='utf-8')
@@ -8,9 +12,11 @@ content = (here / 'README.md').read_text(encoding='utf-8')
 summary = re.search('(?sia)What\?\n----\n(.+?)\n\n', content)[1]
 
 
+
+
 setup(
     name='toraio',
-    version='1.0.5',
+    version='1.0.6',
     url='https://github.com/ultrafunkamsterdam/toraio',
     license='MIT',
     author='UltrafunkAmsterdam',
@@ -19,6 +25,8 @@ setup(
     long_description=content,
     long_description_content_type='text/markdown',
     package_dir={'toraio': 'toraio'},
+    package_data={'toraio': ['bin/linux/Tor/tor', 'bin/linux/*' ,'bin/win32/Tor/tor.exe']
+                  },
     packages=['toraio'],
     include_package_data=True,
     install_requires=[
@@ -26,6 +34,7 @@ setup(
         'aiohttp',
         'aiosocks2',
         'stem',
-    ]
+    ],
+
 
 )

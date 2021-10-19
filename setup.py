8d953d7d20d800a932b1602882d738075ac830ef
UltrafunkAmsterdam <leon@ultrafunk.nl>
2021-10-19 22:56:18 +0200

    1.0.7

--- setup.py
+++ setup.py
@@ -13,31 +13,19 @@ content = (here / 'README.md').read_text(encoding='utf-8')
 summary = re.search('(?sia)What\?\n----\n(.+?)\n\n', content)[1]
 
 
+
+
+
 class postinstall(install):
     """Post-installation for development mode."""
     def run(self):
-        uid, gid = 0, 0
         mode = 0o700
         install.run(self)
-        # calling install.run(self) insures that everything that happened previously still happens, so the installation does not break!
-
-        # here we start with doing our overriding and private magic ..
         for filepath in self.get_outputs():
             exepath = os.path.join('Tor', 'tor')
             if exepath in filepath:
-                log.info('setting %s executable' % filepath)
+                log.warn('setting %s executable' % filepath)
                 os.chmod(filepath, mode)
-
-            # if self.install_scripts in filepath:
-            #
-            #
-            #     log.info("Overriding setuptools mode of scripts ...")
-            #     log.info("Changing ownership of %s to uid:%s gid %s" %
-            #              (filepath, uid, gid))
-            #     os.chown(filepath, uid, gid)
-            #     log.info("Changing permissions of %s to %s" %
-            #              (filepath, oct(mode)))
-            #     os.chmod(filepath, mode)
 
 setup(
     name='toraio',
